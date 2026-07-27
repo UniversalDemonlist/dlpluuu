3eb96e39b16cca46ce2ae69b61f880ec1cd15766
UniversalDemonlist <theactualremake5@gmail.com>
2026-07-27 14:39:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ function renderDemonCards(listOverride) {
   container.innerHTML = "";
   for (let i = 0; i < 6; i++) container.appendChild(createPlaceholderCard());
 
-  let list = listOverride || globalDemons.filter(d => d.position <= 100);
+  let list = listOverride || globalDemons
 
 if (hideCheated) {
   list = list.filter(d => !cheatedList.includes(d.id.toLowerCase()));
