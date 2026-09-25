35f4d8e0da21e047a5a7c43475840e16365a9999
UniversalDemonlist <theactualremake5@gmail.com>
2026-09-25 13:28:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -625,13 +625,15 @@ function createPlayerCard(name, score, rank) {
   const segmentColor = getSegmentColor(t.segment);
 
   const rankColor = {
-    Mythic: "#a020f0",
-    Champion: "#ff00ff",
-    Diamond: "#00bfff",
-    Platinum: "#e5e4e2",
-    Gold: "#ffd700",
-    Silver: "#c0c0c0",
-    Bronze: "#cd7f32"
+   Legend: "#ff3030",
+   Absolute: "#a020f0",
+   Mythic: "#ff00ff",
+   Champion: "#00bfff",
+   Diamond: "#7df9ff",
+   Platinum: "#e5e4e2",
+   Gold: "#ffd700",
+   Silver: "#c0c0c0",
+   Bronze: "#cd7f32"
   }[getPlayerRank(score)];
 
   const hardestName = hardest ? `#${hardest.position} — ${hardest.name}` : "None";
